0d253f134888fe067b6cd2db5902df27a7af07f5
solo4games <78871644+solo4games@users.noreply.github.com>
2021-03-06 07:05:11 -0800

    Add files via upload

--- not_texture_ray.c
+++ not_texture_ray.c
@@ -6,7 +6,7 @@
 /*   By: lbrandy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 12:14:26 by lbrandy           #+#    #+#             */
-/*   Updated: 2021/02/28 17:27:58 by lbrandy          ###   ########.fr       */
+/*   Updated: 2021/03/06 17:18:03 by lbrandy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,7 @@ void	calc_DDA(t_raycast *ray, char** map, t_pos *p)
 		ray->wallDist = (ray->map_y - p->pos_y + (1 - ray->step_y) / 2) / ray->rayDir_y;
 }
 
-void	init_draw(t_draw *draw, t_raycast *r, t_all *all)
+void	init_draw(t_draw *draw, t_raycast *r, t_all *all, t_pos *p)
 {
 	int height;
 	int i;
@@ -94,6 +94,8 @@ void	init_draw(t_draw *draw, t_raycast *r, t_all *all)
 	draw->color = 0;
 	i = 0;
 	height = all->textures->y;
+	draw->tex_width = 64;
+	draw->tex_height = 64;
 	draw->line_h = (int)(height / r->wallDist);
 	draw->start = -draw->line_h / 2 + height / 2;
 	if (draw->start < 0)
@@ -101,15 +103,27 @@ void	init_draw(t_draw *draw, t_raycast *r, t_all *all)
 	draw->end = draw->line_h / 2 + height / 2;
 	if (draw->end >= height)
 		draw->end = height - 1;
-	printf("start - %d end - %d\n; Walldist - %f\n", draw->start, draw->end, r->wallDist);
-	if (all->raycast->rayDir_x < 0 && r->side == 0)
+	printf("start - %d end - %d\n; raydir_x - %f\n", draw->start, draw->end, all->pos->plane_x);
+	if (r->side == 0)
+		draw->wall_x = p->pos_y + r->wallDist * r->rayDir_y;
+	else
+		draw->wall_x = p->pos_x + r->wallDist * r->rayDir_x;
+	draw->wall_x -= floor((draw->wall_x));
+	draw->tex_x = (int)(draw->wall_x * (double)draw->tex_width);
+	if (r->side == 0 && r->rayDir_x > 0)
+		draw->tex_x = draw->tex_width - draw->tex_x - 1;
+	if (r->side == 1 && r->rayDir_y < 0)
+		draw->tex_x = draw->tex_width - draw->tex_x - 1;
+	draw->step = 1.0 * draw->tex_height / draw->line_h;
+	draw->tex_pos = (draw->start - height / 2 + draw->line_h / 2) * draw->step;
+	/*if (all->raycast->rayDir_x < 0 && r->side == 0)
 		draw->color = 0x00FF0000;
 	if (all->raycast->rayDir_x > 0 && r->side == 0)
 		draw->color = 0x0000FF00;
 	if (all->raycast->rayDir_y < 0 && r->side == 1)
     	draw->color = 0x000000FF;
 	if (all->raycast->rayDir_y > 0 && r->side == 1)
-		draw->color = 0x00FF0099;
+		draw->color = 0x00FF0099;*/
 }
 
 void	init_mlx(t_win *w, t_all *all)
@@ -127,7 +141,7 @@ int		drawing(int keycode, t_all *all)
 
 	old_dirx = 0;
 	old_planex = 0;
-	if (keycode == 13)
+	if (keycode == 13)//пофиксить заходы в стену
 	{
 		if(all->map[(int)(all->pos->pos_x + all->pos->dir_x * (all->draw->ms))][(int)all->pos->pos_y] == '0')
 			all->pos->pos_x += all->pos->dir_x * all->draw->ms;
@@ -140,10 +154,6 @@ int		drawing(int keycode, t_all *all)
 			all->pos->pos_x -= all->pos->dir_y * all->draw->ms;
 		if(all->map[(int)(all->pos->pos_x)][(int)(all->pos->pos_y + all->pos->dir_y * (all->draw->ms))] == '0')
 			all->pos->pos_y += all->pos->dir_x * all->draw->ms;
-		/*if(all->map[(int)(p->pos_x + p->dir_x * all->draw->ms),(int)p->pos_y] == '0')
-			p->pos_x += p->dir_x * all->draw->ms;
-		if(all->map[(int)(p->pos_x), (int)(p->pos_y + p->dir_y * all->draw->ms)] == '0')
-			p->pos_y += p->dir_y * all->draw->ms;*/
 	}
 	if(keycode == 1)
 	{
@@ -151,10 +161,6 @@ int		drawing(int keycode, t_all *all)
 			all->pos->pos_x -= all->pos->dir_x * all->draw->ms;
 		if(all->map[(int)(all->pos->pos_x)][(int)(all->pos->pos_y - all->pos->dir_y * (all->draw->ms))] == '0')
 			all->pos->pos_y -= all->pos->dir_y * all->draw->ms;
-		/*if(all->map[(int)(p->pos_x - p->dir_x * all->draw->ms),(int)p->pos_y] == '0')
-			p->pos_x -= p->dir_x * all->draw->ms;
-		if(all->map[(int)(p->pos_x), (int)(p->pos_y - p->dir_y * all->draw->ms)] == '0')
-			p->pos_y -= p->dir_y * all->draw->ms;*/
 	}
 	if (keycode == 2)
 	{
@@ -171,12 +177,6 @@ int		drawing(int keycode, t_all *all)
 		all->pos->dir_y = old_dirx * sin(-all->draw->rs) + all->pos->dir_y * cos(-all->draw->rs);
 		all->pos->plane_x = all->pos->plane_x * cos(-all->draw->rs) - all->pos->plane_y * sin(-all->draw->rs);
 		all->pos->plane_y = old_planex * sin(-all->draw->rs) + all->pos->plane_y * cos(-all->draw->rs);
-		/*old_dirx = p->dir_x;
-		old_planex = p->plane_x;
-		p->dir_x = p->dir_x * cos(-all->draw->rs) - p->dir_y * sin(-all->draw->rs);
-		p->dir_y = old_dirx * sin(-all->draw->rs) + p->dir_y * cos(-all->draw->rs);
-		p->plane_x = p->plane_x * cos(-all->draw->rs) - p->plane_y * sin(-all->draw->rs);
-		p->plane_y = old_planex * sin(-all->draw->rs) + p->plane_y * cos(-all->draw->rs);*/
 	}
 	if (keycode == 123)
 	{
@@ -186,12 +186,6 @@ int		drawing(int keycode, t_all *all)
 		all->pos->dir_y = old_dirx * sin(all->draw->rs) + all->pos->dir_y * cos(all->draw->rs);
 		all->pos->plane_x = all->pos->plane_x * cos(all->draw->rs) - all->pos->plane_y * sin(all->draw->rs);
 		all->pos->plane_y = old_planex * sin(all->draw->rs) + all->pos->plane_y * cos(all->draw->rs);
-		/*old_dirx = p->dir_x;
-		old_planex = p->plane_x;
-		p->dir_x = p->dir_x * cos(all->draw->rs) - p->dir_y * sin(all->draw->rs);
-		p->dir_y = old_dirx * sin(all->draw->rs) + p->dir_y * cos(all->draw->rs);
-		p->plane_x = p->plane_x * cos(all->draw->rs) - p->plane_y * sin(all->draw->rs);
-		p->plane_y = old_planex * sin(all->draw->rs) + p->plane_y * cos(all->draw->rs);*/
 	}
 	draw_frame(all);
 	return(0);
@@ -210,17 +204,18 @@ void	draw_frame(t_all *all)
 		init_raycast(all->raycast, i, all->textures->x, all->pos);
 		init_step(all->raycast, all->pos);
 		calc_DDA(all->raycast, all->map, all->pos);
-		init_draw(all->draw, all->raycast, all);
+		init_draw(all->draw, all->raycast, all, all->pos);
 		while (j < all->draw->start)
 		{
 			my_mlx_pixel_put(all, i, j, 0x00000000);
 			j++;
 		}
-		while(j < all->draw->end)
+		/*while (j < all->draw->end)
 		{
-			my_mlx_pixel_put(all, i, j, all->draw->color);
+			my_mlx_pixel_put(all, i, j, 0x00FF0000);
 			j++;
-		}
+		}*/
+		draw_wall(all, all->draw, &j, i);
 		while (j < all->textures->y)
 		{
 			my_mlx_pixel_put(all, i, j, 0x00000000);
@@ -231,35 +226,39 @@ void	draw_frame(t_all *all)
 	mlx_put_image_to_window(all->win->mlx, all->win->win, all->win->img, 0, 0);
 }
 
+void	draw_wall(t_all *all, t_draw *d, int *y, int x)
+{
+	while (*y < d->end)
+	{
+		d->tex_y = (int)d->tex_pos & (d->tex_height - 1);
+		d->tex_pos += d->step;
+		if (all->raycast->side == 0 && all->raycast->rayDir_x < 0)
+			d->color = (*(int *)(all->SO->addr + (d->tex_x + d->tex_y *
+							all->SO->width) * (all->SO->bpp / 8)));
+		if (all->raycast->side == 0 && all->raycast->rayDir_x > 0)
+			d->color = (*(int *)(all->EA->addr + (d->tex_x + d->tex_y *
+							all->EA->width) * (all->EA->bpp / 8)));
+		if (all->raycast->side == 1 && all->raycast->rayDir_y < 0)
+			d->color = (*(int *)(all->NO->addr + (d->tex_x + d->tex_y *
+							all->NO->width) * (all->NO->bpp / 8)));
+		if (all->raycast->side == 1 && all->raycast->rayDir_y > 0)
+			d->color = (*(int *)(all->WE->addr + (d->tex_x + d->tex_y *
+							all->WE->width) * (all->WE->bpp / 8)));
+		my_mlx_pixel_put(all, x, *y, d->color);
+		(*y)++;
+	}
+}
+
 void	raycasting(t_all *all)
 {
 	int i;
-	//int j;
 
 	i = 0;
-	all->win = (t_win *)malloc(sizeof(t_win));
 	all->raycast = (t_raycast *)malloc(sizeof(t_raycast));
 	all->draw = (t_draw *)malloc(sizeof(t_draw));
 	all->draw->ms = 0.05;
 	all->draw->rs = 0.1;
-	init_mlx(all->win, all);
 	draw_frame(all);
-	//mlx_clear_window(all->win->mlx, all->win->win);
-	/*while (i < all->textures->x)
-	{
-		init_raycast(all->raycast, i, all->textures->x, all->pos);
-		init_step(all->raycast, all->pos);
-		calc_DDA(all->raycast, all->map, all->pos);
-		init_draw(all->draw, all->raycast, all);
-		j = all->draw->start;
-		while(j < all->draw->end)
-		{
-			my_mlx_pixel_put(all, i, j, all->draw->color);
-			j++;
-		}
-		i++;
-	}*/
-	//mlx_put_image_to_window(all->win->mlx, all->win->win, all->win->img, 0, 0);
 	mlx_hook(all->win->win, 2, 1L<<0, &drawing, all);
 	mlx_loop(all->win->mlx);
 }
